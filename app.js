58c1fb1c9434a87b94ff4276d532f820cf634032
Adithya Anandsaikrishnan <anandsaikrishnan.adithya@gmail.com>
2022-07-14 16:52:22 -0700

    added renaming and completed  game

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ const GameBoard = (() => {
     if (!placeSuccess) {
       return false;
     }
-
+    filledSpaces += 1;
     winningRow = _checkForWin();
     if (winningRow[0] === -2) {
       // Game in Progress
@@ -133,6 +133,14 @@ const GameBoard = (() => {
     }
 
     return true;
+  };
+
+  const changeName = (player, newName) => {
+    if (player === 1) {
+      playerList[0].userName = newName;
+    } else {
+      playerList[1].userName = newName;
+    }
   };
 
   return {
@@ -146,28 +154,112 @@ const GameBoard = (() => {
       return gameArray;
     },
     move,
+    changeName,
+    resetGame,
   };
 })();
 
 // displayController Module
 
 const DisplayController = (() => {
+  const clickBoardSetup = () => {};
 
-  
-  const CellList = document.querySelectorAll(".cell");
-  let count = 0;
-  CellList.forEach((cell) => {
-    cell.id = `cell-${count}`;
-    cell.addEventListener("click", () => {
-      console.log(`${cell.id.substring(5)}`);
-      const row = Math.floor(cell.id.substring(5) / 3);
-      const col = cell.id.substring(5) % 3;
+  const initialize = () => {
+    const CellList = document.querySelectorAll(".cell");
+    let count = 0;
+    CellList.forEach((cell) => {
+      cell.id = `cell-${count}`;
+      cell.addEventListener("click", () => {
+        const row = Math.floor(cell.id.substring(5) / 3);
+        const col = cell.id.substring(5) % 3;
 
-      const curPlayerLetter = GameBoard.curPlayer.letter;
-      if (GameBoard.move(col, row)) {
-        cell.innerHTML = `<span>${curPlayerLetter}</span>`;
-      }
+        const curPlayerLetter = GameBoard.curPlayer.letter;
+        const curPlayerName = GameBoard.curPlayer.userName;
+        if (GameBoard.move(col, row)) {
+          cell.innerHTML = `<span>${curPlayerLetter}</span>`;
+
+          if (curPlayerLetter === "X") {
+            const firstPlayer = document.getElementById(
+              "first-playing-background"
+            );
+            const secondPlayer = document.getElementById(
+              "second-playing-background"
+            );
+            secondPlayer.classList.add("current");
+            firstPlayer.classList.remove("current");
+          } else {
+            const firstPlayer = document.getElementById(
+              "first-playing-background"
+            );
+            const secondPlayer = document.getElementById(
+              "second-playing-background"
+            );
+            firstPlayer.classList.add("current");
+            secondPlayer.classList.remove("current");
+          }
+          const winnerArea = document.querySelector(".again-prompt");
+          const winnerText = document.querySelector(".again-prompt h2");
+          if (GameBoard.winningRow[0] === -1) {
+            winnerArea.style.visibility = "visible";
+            winnerText.textContent += "TIE!";
+          } else if (GameBoard.winningRow[0] !== -2) {
+            winnerArea.style.visibility = "visible";
+            winnerText.textContent += `${curPlayerName} wins!`;
+          }
+        }
+      });
+      count++;
     });
-    count++;
-  });
+
+    const firstButton = document.getElementById("one-submit");
+
+    firstButton.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const newInput = document.getElementById("one-name");
+      const newName = newInput.value;
+      GameBoard.changeName(1, newName);
+
+      const nameShow = document.querySelectorAll(".first-name");
+      nameShow.forEach((name) => (name.textContent = `${newName} (X)`));
+      newInput.value = "";
+    });
+
+    const secondButton = document.getElementById("two-submit");
+    secondButton.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const newInput = document.getElementById("two-name");
+      const newName = newInput.value;
+      GameBoard.changeName(2, newName);
+
+      const nameShow = document.querySelectorAll(".second-name");
+      nameShow.forEach((name) => (name.textContent = `${newName} (O)`));
+      newInput.value = "";
+    });
+
+    const startButton = document.querySelector(".start-button");
+    startButton.addEventListener("click", (e) => {
+      const overlay = document.querySelector(".overlay");
+      const first = document.querySelector(".player-one");
+      const second = document.querySelector(".player-two");
+      overlay.style.visibility = "hidden";
+      first.style.visibility = "visible";
+      second.style.visibility = "visible";
+      const firstPlayer = document.getElementById("first-playing-background");
+      firstPlayer.classList.add("current");
+    });
+
+    const restartButton = document.getElementById("again-button");
+    restartButton.addEventListener("click", (e) => {
+      const winnerArea = document.querySelector(".again-prompt");
+      winnerArea.style.visibility = "hidden";
+      GameBoard.resetGame();
+      CellList.forEach((cell) => (cell.textContent = ""));
+      const winnerText = document.querySelector(".again-prompt h2");
+      winnerText.textContent = "Result: ";
+    });
+  };
+
+  initialize();
 })();
